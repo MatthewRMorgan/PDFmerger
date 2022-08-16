53e4aee186747949fdb78a7c096c42d2c2990a4c
MatthewRMorgan <matthewmorgan.r@gmail.com>
2022-08-15 22:41:34 -0700

    init commit

---
+++ combine.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+from PyPDF2 import PdfMerger
+import os
+
+#global var for filenames
+files = []
+
+def UploadAction(event=None):
+    filename = fd.askopenfilename()
+    print('Selected:', filename)
+
+def combine_files():
+    combinedFileName = "combinedPDF.pdf"
+    if (files == []):
+        showinfo("ERROR: no files selected")
+        return 0
+    merger = PdfMerger()
+    for file in files:
+        merger.append(file)
+    merger.write(combinedFileName)
+    merger.close()
+    filetypes = (
+        ('PDF', '*.pdf'),
+    )
+    #savedFile = fd.asksaveasfile(filetypes = filetypes, defaultextension = filetypes)
+    
+    #os.remove(combinedFileName)
+
+def open_files():
+    global files
+    files = []
+    filetypes = (
+        ('PDF', '*.pdf'),
+    )
+
+    filenames = fd.askopenfilenames(
+        title='Open a file',
+        filetypes=filetypes)
+    for i in range(len(filenames)):
+        label = tk.Label(
+            text=filenames[i],
+            fg="black",
+            bg="green",
+            width=20,
+            height=3
+        )
+        label.pack(fill=tk.X)
+        #label.config(text=filename)
+    files = filenames
+
+
+window = tk.Tk()
+window.title("PDF merger")
+#window.geometry('600x500')
+
+frame1 = tk.Frame(master=window, height=100, bg="black")
+frame1.pack(fill=tk.X)
+
+button = tk.Button(
+    text="select PDFs",
+    width=15,
+    height=3,
+    bg="grey",
+    fg="white",
+    command=open_files
+)
+
+
+button2 = tk.Button(
+    text="combine PDFs",
+    width=15,
+    height=3,
+    bg="grey",
+    fg="white",
+    command=combine_files
+)
+label = tk.Label(
+    text="",
+    fg="black",
+    bg="green",
+    width=30,
+    height=10
+)
+
+button.pack(expand=True)
+frame2 = tk.Frame(master=window, height=50, bg="grey")
+#label.pack()
+frame2.pack(fill=tk.X)
+button2.pack()
+
+window.mainloop()
+
+print("goodbye")
+print(files)
